91fe3525774ad1607cf5b2f3f1be523cd45a1bd6
tibetangodd-prog <tibetangodd@gmail.com>
2026-07-06 16:30:55 +0800

    Add TypeConverters for Category in Room database

---
+++ app/src/main/java/com/calendarapp/simple/data/Converters.kt
@@ -0,0 +1,11 @@
+package com.calendarapp.simple.data
+
+import androidx.room.TypeConverter
+
+class Converters {
+    @TypeConverter
+    fun fromCategory(category: Category): String = category.name
+
+    @TypeConverter
+    fun toCategory(value: String): Category = Category.valueOf(value)
+}
